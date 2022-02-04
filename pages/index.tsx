69b9c34ad2327550e685f8ee6eae1bea468e5a2f
rampage0131 <jake.docker0131@gmail.com>
2022-02-04 15:37:57 +0545

    Build Error fixed !!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -60,9 +59,6 @@ const Home: NextPage = () => {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
         </a>
       </footer>
     </div>
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Test: NextPage = () => {
+    return (
+        <div>
+            <h1>NEW PAGE !!</h1>
+        </div>
+    )
+}
+
+export default Test;
