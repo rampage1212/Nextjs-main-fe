5f69f337bd925f0fa3fe321039e8b8dd1b05535a
rampage0131 <jake.docker0131@gmail.com>
2022-02-04 16:41:04 +0545

    export command added in package.json !!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -64,7 +64,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- pages/test.tsx
+++ pages/test.tsx
@@ -2,12 +2,14 @@ import { NextPage } from "next";
 import Link from "next/link";
 
 const Test: NextPage = () => {
+    const x = 10;
+
     return (
         <div>
             <h1>NEW PAGE !!</h1>
             <h3><Link href={"/"}>Back !!</Link></h3>
         </div>
-    )
-}
+    );
+};
 
 export default Test;
