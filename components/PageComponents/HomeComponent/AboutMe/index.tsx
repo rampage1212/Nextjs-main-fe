ce9af6057e02b9afbbd07ba1bcf52c156209bef1
rampage0131 <jake.docker0131@gmail.com>
2022-02-11 13:14:02 +0545

    Mobile view added for all pages !!

--- components/PageComponents/HomeComponent/AboutMe/index.tsx
+++ components/PageComponents/HomeComponent/AboutMe/index.tsx
@@ -32,55 +32,66 @@ const AboutMe = (props: homeComponentsPropsDataType | AboutMePagesComponentProps
                         }
 
                         <div className={`${aboutMeStyles.section_body}`}>
-                            <div className={aboutMeStyles.data_section}>
-                                <p>
-                                    Hi! my name is Bhaskar Neupane. I am a recent A-Levels graduate and a Full-Stack software developer based in Kathmandu, Nepal. My passion for programming started back in 2015 when I wrote my first proper program using QBasic to solve quadratic equations. Turns out making computer do my maths homework was a fun place to start my journey as a problem solver.
-                                </p>
-                                <p>
-                                    Currently, I am working as a Full-Stack Developer using several Technologies. Additionally, I am learning about Machine and Deep Learning and am looking forward to learn hardware side of things too.
-                                </p>
-                                <p>
-                                    I love to capture memories, it helps to break pattern and helps me stay creative and have a fresh mind. I have few images that I have captured in my <a href="https://instagram.com/vaskrneup" target={"_blank"} className={utilsStyles.link} rel="noreferrer">instagram</a>.
-                                </p>
-
-                                <p className={aboutMeStyles.technologies_title}>
-                                    Here are few Technologies I&apos;ve worked with:
-                                </p>
-
-                                <div className={aboutMeStyles.technologies_grid}>
-                                    {
-                                        props.aboutMeData
-                                            .map((data: technologiesWorkedWithPropsDataType) => {
-                                                return (
-                                                    <a
-                                                        href={data.url}
-                                                        key={data.url}
-                                                        target={"_blank"}
-                                                        rel="noreferrer"
-                                                    >
-                                                        <TechnologyViewCard
-                                                            image={data.icon}
-                                                            imageAltText={`${data.name} Icon`}
-                                                            name={data.name}
-                                                        />
-                                                    </a>
-                                                );
-                                            })
-                                    }
+                            <div className={aboutMeStyles.image_top}>
+                                <div className={`${aboutMeStyles.image_section}`}>
+                                    <img
+                                        src="https://pbs.twimg.com/profile_images/1265216954408689665/IkEH5P_j_400x400.jpg"
+                                        alt="Bhaskar Neupane"
+                                    />
                                 </div>
                             </div>
 
-                            <div className={aboutMeStyles.image_section}>
-                                <img
-                                    src="https://pbs.twimg.com/profile_images/1265216954408689665/IkEH5P_j_400x400.jpg"
-                                    alt="Bhaskar Neupane"
-                                />
+                            <div className={aboutMeStyles.section_body_main}>
+                                <div className={aboutMeStyles.data_section}>
+                                    <p>
+                                        Hi! my name is Bhaskar Neupane. I am a recent A-Levels graduate and a Full-Stack software developer based in Kathmandu, Nepal. My passion for programming started back in 2015 when I wrote my first proper program using QBasic to solve quadratic equations. Turns out making computer do my maths homework was a fun place to start my journey as a problem solver.
+                                    </p>
+                                    <p>
+                                        Currently, I am working as a Full-Stack Developer using several Technologies. Additionally, I am learning about Machine and Deep Learning and am looking forward to learn hardware side of things too.
+                                    </p>
+                                    <p>
+                                        I love to capture memories, it helps to break pattern and helps me stay creative and have a fresh mind. I have few images that I have captured in my <a href="https://instagram.com/vaskrneup" target={"_blank"} className={utilsStyles.link} rel="noreferrer">instagram</a>.
+                                    </p>
+
+                                    <p className={aboutMeStyles.technologies_title}>
+                                        Here are few Technologies I&apos;ve worked with:
+                                    </p>
+
+                                    <div className={aboutMeStyles.technologies_grid}>
+                                        {
+                                            props.aboutMeData
+                                                .map((data: technologiesWorkedWithPropsDataType) => {
+                                                    return (
+                                                        <a
+                                                            href={data.url}
+                                                            key={data.url}
+                                                            target={"_blank"}
+                                                            rel="noreferrer"
+                                                        >
+                                                            <TechnologyViewCard
+                                                                image={data.icon}
+                                                                imageAltText={`${data.name} Icon`}
+                                                                name={data.name}
+                                                            />
+                                                        </a>
+                                                    );
+                                                })
+                                        }
+                                    </div>
+                                </div>
+
+                                <div className={`${aboutMeStyles.image_section} ${aboutMeStyles.image_bottom}`}>
+                                    <img
+                                        src="https://pbs.twimg.com/profile_images/1265216954408689665/IkEH5P_j_400x400.jpg"
+                                        alt="Bhaskar Neupane"
+                                    />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </Section>
+                    </div >
+                </div >
+            </div >
+        </Section >
     );
 };
 
